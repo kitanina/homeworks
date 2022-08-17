26424612a484adef7f471824cc65325879e3a083
Elena Kitanina <elena.kitanina@stenn.com>
2022-08-17 16:01:27 +0400

    Задание. Вам дан словарь per_cent с распределением процентных ставок по вкладам в различных банках таким образом, что ключ — название банка, значение — процент. Напишите программу, в результате которой будет сформирован список deposit значений — накопленные средства за год вклада в каждом из банков. На вход программы с клавиатуры вводится сумма money, которую человек планирует положить под проценты.
    per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
    Пример работы программы при
    
    money = 100000
    deposit = [5600, 5900, 4280, 4000]
    Для самостоятельного изучения вам была дана ссылка на методы для работы со списками. Изучите методы и найдите тот, который позволяет найти максимальное значение среди элементов в списке.
    
    Добавьте в программу поиск максимального значения и его вывод на экран в формате:
    
    Максимальная сумма, которую вы можете заработать — deposit[i]
    Где вместо deposit[i] будет выведено максимальное значение списка.
    
    Для проверки загрузите полученное решение на GitHub и прикрепите ссылку.

---
+++ 12.7.3(HW-03).py
@@ -0,0 +1,12 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+per_cent = list[float](per_cent.values())
+money = float(input('Enter the sum you want to deposit:')) / 100
+
+for i, value in enumerate(per_cent):
+    per_cent[i] = value * money
+deposit = per_cent
+print(deposit)
+print('The max sum you could earn:', max(deposit))
+
+("deposit_per_year = float(per_cent.values()) * money / 100\n"
+ "print(deposit_per_year)")
